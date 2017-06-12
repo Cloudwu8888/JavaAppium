a56f59cd3600ad704e34f8746634d97ed4b81e5b
Rex Song <westerncampus@gmail.com>
2017-06-13 00:18:50 +0800

    init commit

---
+++ src/test/java/com/test/sample/AndroidTest.java
@@ -0,0 +1,25 @@
+package com.test.sample;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+/**
+ * Created by RexSo on 6/12/2017.
+ */
+public class AndroidTest extends BaseDriver {
+    @Test
+    public void apiDemo(){
+        WebElement el = driver.findElement(By.xpath(".//*[@text='Animation']"));
+        assertEquals("Animation", el.getText());
+        el = driver.findElementByClassName("android.widget.TextView");
+        assertEquals("API Demos", el.getText());
+        el = driver.findElement(By.xpath(".//*[@text='App']"));
+        el.click();
+        List<WebElement> els = driver.findElementsByClassName("android.widget.TextView");
+        assertEquals("Activity", els.get(2).getText());
+    }
+}
